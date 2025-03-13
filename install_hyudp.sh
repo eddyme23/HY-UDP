23dbd24f4a04a0264c07f4646e995c0d5b6473bc
eddyme23 <saxonyedward@gmail.com>
2025-03-13 10:57:18 +0000

    Update install_hyudp.sh

--- install_hyudp.sh
+++ install_hyudp.sh
@@ -22,10 +22,10 @@ PROTOCOL="udp"
 UDP_PORT=":36712"
 
 # OBFS
-OBFS="eddyvpn"
+OBFS="GuruzXvpn"
 
 # PASSWORDS
-PASSWORD="eddyvpn"
+PASSWORD="GuruzXvpn"
 
 # Basename of this script
 SCRIPT_NAME="$(basename "$0")"
