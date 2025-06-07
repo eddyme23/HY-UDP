e152926ecc6b8e5d743eafcd10da490e276f5b38
eddyme23 <saxonyedward@gmail.com>
2025-06-07 18:24:08 +0000

    Update install_hyudp.sh

--- install_hyudp.sh
+++ install_hyudp.sh
@@ -22,10 +22,10 @@ PROTOCOL="udp"
 UDP_PORT=":36712"
 
 # OBFS
-OBFS="vpnxXEdyln"
+OBFS="ndcgh6VPN"
 
 # PASSWORDS
-PASSWORD="vpnxXEdyln"
+PASSWORD="ndcgh6VPN"
 
 # Basename of this script
 SCRIPT_NAME="$(basename "$0")"
