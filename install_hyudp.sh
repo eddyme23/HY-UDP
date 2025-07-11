77475246b2ca07cc6991bfe228cad7b3446ff773
eddyme23 <saxonyedward@gmail.com>
2025-07-11 14:35:03 +0000

    Update install_hyudp.sh

--- install_hyudp.sh
+++ install_hyudp.sh
@@ -22,10 +22,10 @@ PROTOCOL="udp"
 UDP_PORT=":36712"
 
 # OBFS
-OBFS="za2uhy"
+OBFS="sa2uhy"
 
 # PASSWORDS
-PASSWORD="ExUdp25hy"
+PASSWORD="EzUdp25hy"
 
 # Basename of this script
 SCRIPT_NAME="$(basename "$0")"
